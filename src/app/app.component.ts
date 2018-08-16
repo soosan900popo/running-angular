62cc7a4e44608ec8b6e0a6969cc65609a18075d1
soosan900popo <soosan900popo@gmail.com>
2018-08-16 22:11:32 +0900

    템플릿 만들기

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'my-app',
+  template: '<p>Hello World!</p>',
+  /* template: `
+  <h1>Hello World!</h1>
+  `, */
+  styles: [`
+        p {
+          color: yellow;
+          font-size: 25px;
+        }
+    `]
+  /* styleUrls: ['./app.component.css'] */
+  /* template: ...
+  styles: ...
+  viewProviders: ... */
 })
+/* 여러 줄 템플릿(template) 또는 스타일시트(styles)에는 역따옴표를 사용 */
 export class AppComponent {
-  title = 'first';
+  title = 'Chapter 4 Intro';
 }
