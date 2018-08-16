d9b580c62f0c4c78186af71708c683efe3b801ad
soosan900popo <soosan900popo@gmail.com>
2018-08-16 22:42:40 +0900

    생성자 활용

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,19 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  template: '<span>Hello my name is Brendan</span>',
-  /* template: `
-  <h1>Hello World!</h1>
-  `, */
-  styles: [`
-        span {
-          font-weight: bold;
-          border: 1px ridge blue;
-          padding:5px;
-        }
-    `]
+  selector: 'simple-constructor',
+  template: `
+    <p>Hello today is {{today}}`,
   /* styleUrls: ['./app.component.css'] */
-  /* template: ...
+  /* templateUrls: ...
   styles: ...
   viewProviders: ... */
 })
 /* 여러 줄 템플릿(template) 또는 스타일시트(styles)에는 역따옴표를 사용 */
-export class AppComponent {
-  title = 'Chapter 4 Intro';
+
+export class UsingAContstructor {
+  today: Date
+  constructor() {
+    this.today = new Date();
+  }
 }
