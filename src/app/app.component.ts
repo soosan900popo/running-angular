e45e6f28a6c03f3571add8bb5ed7eeb76b3727eb
soosan900popo <soosan900popo@gmail.com>
2018-08-20 01:20:24 +0900

    속성(Attribute) 디렉티브 ★★★

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,70 +2,31 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <div *ngIf="condition">condition met</div>
-    <div *ngIf="!condition">condition not met</div>
-    <button (click)="changeCondition()">Change Condition</button>
-    <hr>
-    <div *ngFor="let person of people">
-      name: {{person}}
-    </div>
-    <hr>
-    <h3>Monsters and where they live</h3>
-    <ul *ngFor="let monster of monsters">
-        {{monster.name}}:  
-        {{monster.location}}
-    </ul>
-    <hr>
-    <div [ngSwitch]="time">
-      <span *ngSwitchCase="'night'">Its night time
-      <button (click)="changeDay()">change to day</button>
-      </span>
-      <span *ngSwitchDefault>Its day time
-      <button (click)="changeNight()">change to night</button></span>
-    </div>
-  `
-})
-/* ***************
-angular5 not error
-***************
-<template ngFor let-person [ngForOf]="people">
-  <div>name: {{person}}</div>
-</template> 
-  | |
-  \ /
-<div *ngFor="let person of people">
-  name: {{person}}
-</div>
-*/
-
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+ })
 export class AppComponent {
-  condition: boolean = true;
-  changeCondition = function () {
-    this.condition = !this.condition;
-  }
-  changeDay = function () {
-    this.time = 'day';
-  }
-  changeNight = function () {
-    this.time = 'night'
-  }
-  people: string[] = [
-    "Andrew", "Dillon", "Philipe", "Susan"
-  ]
-  monsters = [
-    {
-      name: "Nessie",
-      location: "Loch Ness, Scotland"
-    },
-    {
-      name: "Bigfoot",
-      location: "Pacific Northwest, USA"
-    },
-    {
-      name: "Godzilla",
-      location: "Tokyo, sometimes New York"
+    colors: string[] = ["red", "blue", "green", "yellow"];
+    weapon: string;
+    charName: string;
+    color: string = 'color';
+    isDisabled: boolean = true;
+    classes:string[] = ['bold', 'italic', 'highlight'];
+    selectedClass:string[] = [];
+    enabler(){
+      this.isDisabled = !this.isDisabled;
     }
-  ]
-  time: string = 'night';
+    addClass(event: any){
+      this.selectedClass = [];
+      var values = event.target.options;
+      var opt: any;
+
+      for (var i=0, iLen = values.length; i<iLen; i++){
+        opt = values[i];
+
+        if (opt.selected){
+          this.selectedClass.push(opt.text);
+        }
+      }
+    }
 }
