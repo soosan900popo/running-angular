18a5366040aef643d343c4b12cbc7bb147e6abaa
soosan900popo <soosan900popo@gmail.com>
2018-08-19 17:02:50 +0900

    프로퍼티 바인딩

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,31 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    {{str1 + ' ' + name}}
+    <img [src]="myPic"/>
     <br>
-    <img src="{{imageSrc}}" />
-    <br>
-    <p>{{str2 + getLikes(likes)}}</p>
+    <button [disabled]="isEnabled">Click me</button><hr>
+    <button disabled="{!isEnabled}">Click me</button><br>
+    <p [ngClass]="className">This is cool stuff</p>
   `,
   styles: [`
-    img{
-      width: 300px;
-      height: auto;
+    img {
+      height: 100px;
+      width auto;
     }
-    p{
-      font-size: 35px;
-      color: darkBlue;
+    .myClass {
+      color: red;
+      font-size: 24px;
     }
   `]
 })
 export class AppComponent {
-  str1: string = "Hello my name is"
-  name: string = "Brendan"
-  str2: string = "I like to"
-  likes: string[] = ['hike', "rappel", "Jeep"]
-  getLikes = function(arr: any){
-    var arrString = arr.join(", ");
-    return " " + arrString
-  }
-  imageSrc: string = "../assets/images/angelsLanding.jpg"
+  myPic: string = "../assets/images/sunset.jpg";
+  isEnabled: boolean = false;
+  className: string = "myClass";
 }
