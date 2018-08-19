124fac4266904f90726146e22060170035850053
soosan900popo <soosan900popo@gmail.com>
2018-08-20 00:46:26 +0900

    구조(Structural) 디렉티브

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,70 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div *ngIf="condition">condition met</div>
+    <div *ngIf="!condition">condition not met</div>
+    <button (click)="changeCondition()">Change Condition</button>
+    <hr>
+    <div *ngFor="let person of people">
+      name: {{person}}
+    </div>
+    <hr>
+    <h3>Monsters and where they live</h3>
+    <ul *ngFor="let monster of monsters">
+        {{monster.name}}:  
+        {{monster.location}}
+    </ul>
+    <hr>
+    <div [ngSwitch]="time">
+      <span *ngSwitchCase="'night'">Its night time
+      <button (click)="changeDay()">change to day</button>
+      </span>
+      <span *ngSwitchDefault>Its day time
+      <button (click)="changeNight()">change to night</button></span>
+    </div>
+  `
 })
+/* ***************
+angular5 not error
+***************
+<template ngFor let-person [ngForOf]="people">
+  <div>name: {{person}}</div>
+</template> 
+  | |
+  \ /
+<div *ngFor="let person of people">
+  name: {{person}}
+</div>
+*/
+
 export class AppComponent {
-  title = 'first';
+  condition: boolean = true;
+  changeCondition = function () {
+    this.condition = !this.condition;
+  }
+  changeDay = function () {
+    this.time = 'day';
+  }
+  changeNight = function () {
+    this.time = 'night'
+  }
+  people: string[] = [
+    "Andrew", "Dillon", "Philipe", "Susan"
+  ]
+  monsters = [
+    {
+      name: "Nessie",
+      location: "Loch Ness, Scotland"
+    },
+    {
+      name: "Bigfoot",
+      location: "Pacific Northwest, USA"
+    },
+    {
+      name: "Godzilla",
+      location: "Tokyo, sometimes New York"
+    }
+  ]
+  time: string = 'night';
 }
