e8cf25023b38aee401a232c7c66e842051c31d95
soosan900popo <soosan900popo@gmail.com>
2018-08-19 17:09:48 +0900

    클래스 바인딩

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,25 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <img [src]="myPic"/>
-    <br>
-    <button [disabled]="isEnabled">Click me</button><hr>
-    <button disabled="{!isEnabled}">Click me</button><br>
-    <p [ngClass]="className">This is cool stuff</p>
+    <div [class]="myCustomClass"></div>
+    <span [class.redText]="isTrue">Hello my blue friend</span>
   `,
   styles: [`
-    img {
-      height: 100px;
-      width auto;
+    .blueBox {
+      height: 150px;
+      width: 150px;
+      background-color: blue;
     }
-    .myClass {
+    .redText{
       color: red;
       font-size: 24px;
     }
-  `]
+  `] 
 })
 export class AppComponent {
-  myPic: string = "../assets/images/sunset.jpg";
-  isEnabled: boolean = false;
-  className: string = "myClass";
+  myCustomClass: string = 'blueBox';
+  isTrue = true;
 }
