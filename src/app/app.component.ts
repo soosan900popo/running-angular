2d5558b637c0bb3bc6885b019f2b9038f23ceb98
soosan900popo <soosan900popo@gmail.com>
2018-08-20 03:43:43 +0900

    중첩 컴포넌트에서 사용자 정의 이벤트 구현

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'first';
+  text: string = '';
+  eventHandler(event: any) {
+    this.text = event;
+  }
+
 }
