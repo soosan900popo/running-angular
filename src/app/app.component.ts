d6c12abe1264216f810452a8b63199f43d087c54
soosan900popo <soosan900popo@gmail.com>
2018-08-20 02:41:17 +0900

    컴포넌트를 이용한 사용자 정의 디렉티브 만들기 ★★★

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  images: string[] = [
-    '../assets/images/jump.jpg',
-    '../assets/images/flower2.jpg',
-    '../assets/images/cliff.jpg'
+
+  images: any = [
+    {
+      src: "../assets/images/angelsLanding.jpg",
+      title: "Angels Landing",
+      description: "A natural wonder in Zion National Park Utah, USA"
+    },
+    {
+      src: "../assets/images/pyramid.jpg",
+      title: "Tikal",
+      description: "Mayan Ruins, Tikal Guatemala"
+    },
+    {
+      src: "../assets/images/sunset.jpg"
+    },
   ]
 }
