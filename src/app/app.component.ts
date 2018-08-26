c07abcf8500b295d1876134ce753d3037a8dbcd2
soosan900popo <soosan900popo@gmail.com>
2018-08-26 21:54:09 +0900

    애니메이션 서비스를 사용하는 앵귤러 애플리케이션 구현하기

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
+import { AnimatedComponent } from './animated/animated.component';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'first';
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { AnimatedComponent } from './animated/animated.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnimatedComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
