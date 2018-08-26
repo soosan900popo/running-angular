a7ac015ac28cafd7015179753fbcfce6d8faaa1c
soosan900popo <soosan900popo@gmail.com>
2018-08-26 22:17:25 +0900

    드래그 앤드 드롭이 가능한 앵귤러 애플리케이션 구현하기

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
-import { ZoomitComponent } from './zoomit/zoomit.component';
+import { DragItemComponent} from './drag-item/drag-item.component';
+import { DropItemComponent} from './drop-item/drop-item.component';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  tagList: string[];
+  constructor() {    
+    this.tagList = [
+      'Nature',
+      'Landscape',
+      'Flora',
+      'Sunset',
+      'Desert',
+      'Beauty',
+      'Inspiring',
+      'Summer',
+      'Fun'
+    ]
+  }
+  ngOnInit() {
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ZoomitComponent } from './zoomit/zoomit.component';
+import { DragItemComponent } from './drag-item/drag-item.component';
+import { DropItemComponent } from './drop-item/drop-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ZoomitComponent
+    DragItemComponent,
+    DropItemComponent
   ],
   imports: [
     BrowserModule
