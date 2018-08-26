5937f28dd7c111befed6b12081db43b808748a4e
soosan900popo <soosan900popo@gmail.com>
2018-08-26 20:02:35 +0900

    데이터 변환 서비스 구현하기

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,42 @@
-import { Component, OnInit } from '@angular/core';
-import { PiService } from './pi.service';
+import { Component } from '@angular/core';
+import { AreaCalcService } from './area-calc.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  providers: [ PiService ]
+  styleUrls: ['./app.component.css'],
+  providers: [AreaCalcService]
 })
-export class AppComponent implements OnInit {
-  pi: number;
-  constructor(
-    private PiService: PiService
-  ){}
-  ngOnInit(){
-    this.pi = this.PiService.getPi();
+export class AppComponent {
+  circleRadius: number = 0;
+  squareBase: number = 0;
+  rectangleBase: number = 0;
+  rectangleHeight: number = 0;
+  triangleBase: number = 0;
+  triangleHeight: number = 0;
+  trapezoidBase1: number = 0;
+  trapezoidBase2: number = 0;
+  trapezoidHeight: number = 0;
+
+  constructor(private areaCalc: AreaCalcService) {}
+
+  doCircle() {
+    return this.areaCalc.circle(this.circleRadius);
+  }
+  doSquare() {
+    return this.areaCalc.square(this.squareBase);
+  }
+  doRectangle() {
+    return this.areaCalc.rectangle(this.rectangleBase, this.rectangleHeight);
+  }
+  doTriangle() {
+    return this.areaCalc.triangle(this.triangleBase, this.triangleHeight);
+  }
+  doTrapezoid() {
+    return this.areaCalc.trapezoid(
+      this.trapezoidBase1,
+      this.trapezoidBase2,
+      this.trapezoidHeight
+    );
   }
 }
